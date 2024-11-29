10044b39b593e791ac8a38cdc1916dee877e7408
frankonofa <franklin.onofa@uisek.edu.ec>
2024-11-29 10:32:12 -0500

    Cambio TH
    Servicios catalogos categoria, titularidad, forma pago

---
+++ Infraestructure/Repository/Core/TitularidadEmpRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entidades.Core;
+using Core.Interfaces.Core;
+using Infraestructure.Configuration.Zeus.Core;
+using Infraestructure.Repository.Generico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repository.Core
+{
+    public class TitularidadEmpRepository : GenericCoreRepository<TipoContratoN>, ITitularidadEmpRepository
+    {
+        public TitularidadEmpRepository(ZeusCoreContext context) : base(context)
+        {
+
+        }
+    }
+}
