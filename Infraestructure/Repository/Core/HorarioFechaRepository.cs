b8422e149792e618d2a433d367d1c0092670ea2b
CristhianT3793 <cristhian.tupiza@uisek.edu.ec>
2025-06-04 15:36:00 -0500

    arreglo orden fechas modulares

--- Infraestructure/Repository/Core/HorarioFechaRepository.cs
+++ Infraestructure/Repository/Core/HorarioFechaRepository.cs
@@ -67,7 +67,7 @@ namespace Infraestructure.Repository.Core
         {
             List<HorarioModularDto> lsthorario = new List<HorarioModularDto>();
             HorarioModularDto horario = new HorarioModularDto();
-            DataSet ds_horario = Conexion.BuscarZEUS_ds("HORARIO_FECHA", "ID_ESPACIOS_FISICOS, ID_PLANIFICACION,ORDEN_FECHA,FECHA AS FECHA_INI,FECHA AS FECHA_FIN,HORA_INI AS HORA_INI,HORA_FIN AS HORA_FIN ", "where ID_PLANIFICACION="+idplanificacion+ " ORDER BY ORDEN_FECHA ASC,HORA_INI");
+            DataSet ds_horario = Conexion.BuscarZEUS_ds("HORARIO_FECHA", "ID_ESPACIOS_FISICOS, ID_PLANIFICACION,ORDEN_FECHA,FECHA AS FECHA_INI,FECHA AS FECHA_FIN,HORA_INI AS HORA_INI,HORA_FIN AS HORA_FIN ", "where ID_PLANIFICACION="+idplanificacion+ " ORDER BY FECHA ,HORA_INI asc");
             if (ds_horario.Tables[0].Rows.Count > 0)
             {
                 foreach (DataRow row in ds_horario.Tables[0].Rows)
