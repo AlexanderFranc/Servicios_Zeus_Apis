5260ec9c53631958e701f360e119754f71b73301
lobo1303 <edwin.villalobos@uisek.edu.ec>
2025-07-11 15:18:03 -0500

    ADD: aumento relacionFamController

---
+++ Servicios_Zeus/Controllers/Core/RelacionFamController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Core.Entidades.Core;
+using Core.Interfaces.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Servicios_Zeus.Helpers.Errors;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Servicios_Zeus.Controllers.Core
+{
+    //[Authorize]
+    [ApiVersion("1.0")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public class RelacionFamController : ControllerBase
+    {
+        private readonly IRelacionFamRepository _relacionFam;
+        private readonly IMapper _mapper;
+        public RelacionFamController(IRelacionFamRepository relacionFam, IMapper mapper)
+        {
+            _relacionFam = relacionFam;
+            _mapper = mapper;
+        }
+        [Route("listar")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RelacionFam>>> GetAllRelacionFam()
+        {
+            var items = await _relacionFam.GetAllAsync();
+            if (items == null)
+                return NotFound(new ApiResponse(404, "La lista no contiene ningún elemento."));
+            return Ok(items);
+        }
+
+        [Route("ver/{id}")]
+        [HttpGet]
+        public async Task<ActionResult<RelacionFam>> GetRelacionFamById(int id)
+        {
+            var items = await _relacionFam.GetByIdAsync(id);
+            if (items == null)
+                return NotFound(new ApiResponse(404));
+            return Ok(items);
+        }
+    }
+}
